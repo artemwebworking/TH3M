f194ef2dabf2cb06d263189d01d7a6b3e9893004
artemwebworking <artemwebworking@gmail.com>
2021-09-26 23:53:21 +0300

    marquee

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,17 @@ window.$ = $
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
-
+require('~/app/js/jquery.marquee.min.js')
 document.addEventListener('DOMContentLoaded', () => {
 	
 	// Custom JS
+//margquee
+$('.marquee').marquee({
+	duration: 30000,
+	gap: 0,
+	duplicated: true,
+	startVisible: true,
+	direction: 'right',
+})
 
 })
