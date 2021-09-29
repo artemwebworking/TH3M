9ca5940b73def44c8fb1e1160d7f222558017007
artemwebworking <artemwebworking@gmail.com>
2021-09-29 16:30:09 +0300

    working with hovers

--- app/js/app.js
+++ app/js/app.js
@@ -5,17 +5,22 @@ window.$ = $
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
+
+
 require('~/app/js/jquery.marquee.min.js')
 document.addEventListener('DOMContentLoaded', () => {
 	
 	// Custom JS
 //margquee
 $('.marquee').marquee({
-	duration: 30000,
+	duration: 3000,
 	gap: 0,
 	duplicated: true,
 	startVisible: true,
-	direction: 'right',
+	direction: 'up',
 })
-
+$('.opening-content-bottom-description-sound').on('click', function() {
+    $('.opening-content-bottom-description-sound').toggleClass('active'),
+	$('.opening-bgr-video').attr('muted');
+})
 })
